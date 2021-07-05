e4744abb75ddf541a2344c55ef6e1d8559e45f0c
wildhamster26 <38760849+wildhamster26@users.noreply.github.com>
2021-07-05 09:54:57 +0200

    Feat: create skeleton form

--- src/WelcomePage/WelcomePage.jsx
+++ src/WelcomePage/WelcomePage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const WelcomePage = () => {
-  return <div>welcome page</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+
+  return (
+    <div>
+      <h1>Welcome friend!</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input />
+        <button type="submit">Start game</button>
+      </form>
+    </div>
+  );
 };
 
 export default WelcomePage;
