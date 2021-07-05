bfc48ba4c1c6f73edf810fbc4edc4ae5f0b7469c
wildhamster26 <38760849+wildhamster26@users.noreply.github.com>
2021-07-05 09:51:11 +0200

    Feat: create skeleton WelcomePage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
 import "./App.css";
+import WelcomePage from "./WelcomePage/WelcomePage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <WelcomePage />
+    </div>
+  );
 }
 
 export default App;
