680def269e42b0e9084929c1033852b0665e717f
wildhamster26 <38760849+wildhamster26@users.noreply.github.com>
2021-07-05 11:09:17 +0200

    Feat: add basic click behavior to lightbulbs

--- src/GamePage/children/GameBoard/GameBoard.jsx
+++ src/GamePage/children/GameBoard/GameBoard.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./GameBoard.css";
 
 const GameBoard = ({ setScore }) => {
-  const colors = ["yellow", "green", "purple", "orange", "red", "blue"];
+  const [selected, setSelected] = useState(undefined);
+
+  const colors = [
+    { name: "yellow", hexcode: "#FDFD97" },
+    { name: "green", hexcode: "#9EE09E" },
+    { name: "purple", hexcode: "#CC99C9" },
+    { name: "orange", hexcode: "#FEB144" },
+    { name: "red", hexcode: "#FF6663" },
+    { name: "blue", hexcode: "#9EC1CF" },
+  ];
 
   return (
     <>
       <div className="board">
         {colors.map((color) => (
-          <div className="lightbulb">{color}</div>
+          <div
+            key={color.name}
+            className="lightbulb"
+            style={{
+              backgroundColor: selected === color.name && color.hexcode,
+            }}
+            onClick={() => setSelected(color.name)}
+          />
         ))}
       </div>
     </>
